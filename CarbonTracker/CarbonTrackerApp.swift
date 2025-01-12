b2aa9c1dfbc08fc6c630ca7a6e9e458d7dd0a275
Maya Griffith <mayegriffith@gmail.com>
2025-01-11 22:35:57 -0500

    Initial Commit

---
+++ CarbonTracker/CarbonTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarbonTrackerApp.swift
+//  CarbonTracker
+//
+//  Created by Maya Griffith on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CarbonTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarbonTrackerTests/CarbonTrackerTests.swift
@@ -0,0 +1,16 @@
+//
+//  CarbonTrackerTests.swift
+//  CarbonTrackerTests
+//
+//  Created by Maya Griffith on 1/11/25.
+//
+
+import Testing
+
+struct CarbonTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
